02283d301d12c0ab696a710b69228833812511a1
anuragb26 <anuragb26@gmail.com>
2018-01-19 02:13:21 +0530

    Complete video app

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -16,17 +16,17 @@ class SearchBar extends Component {
 	render(){
 		// return <input onChange={this.onInputChange} />;
 		return (
-			<div> 	
+			<div className='search-bar'> 	
 				<input
 				value={this.state.term}
-				onChange = {event => this.setState({'term':event.target.value})} />
-				Value of the input : {this.state.term}
+				onChange = {event => this.onInputChange(event.target.value)} />
 			</div>
 			);
 	}
 
-	onInputChange(event){
-		console.log(event.target.value);
+	onInputChange(term){
+		this.setState({term});
+		this.props.onSearchTermChange(term);
 	}
 }
 
